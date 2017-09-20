495eb0c5d1fb01d9281fe74dabfb099e4c281a2c
Zhongyang.Wu <zhongyang.wu@outlook.com>
2017-09-20 16:36:09 +0800

    Initial commit

---
+++ wasserstein-gan.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
